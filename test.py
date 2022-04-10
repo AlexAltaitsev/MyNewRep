2d9b715076fa8f80103bbc15c2b22e5bce2413dd
vase031 <iwonty8@gmail.com>
2022-04-10 15:56:27 +0300

    v.01

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print('Nok nok')
+from json.tool import main
+
+
+class Armour(object):
+    
+    def is_penetrated(self,thickness, calibr):
+        if calibr * 3 > thickness:
+            bool = "yes"
+            return print(bool)
+        else:
+            bool = "no"
+            return print(bool) 
+
+
+q = Armour()
+q = q.is_penetrated(20,5)
+
+if main == __name__:
+    print(q)
